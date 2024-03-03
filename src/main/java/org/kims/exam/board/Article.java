c20edd6a2af31116bee8b4c84b9a0900e2ef03e1
neogoolboss <krossjsnet88@gmail.com>
2024-03-03 21:12:42 +0900

    toString 메서드를 오버라이드 하여 Article 객체가 문장으로서 출력되도록 함

--- src/main/java/org/kims/exam/board/Article.java
+++ src/main/java/org/kims/exam/board/Article.java
@@ -6,7 +6,44 @@ public class Article {
     String title;
     String body;
 
+    public Article() {
+    }
 
+    public Article(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return
+                String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
+
+    }
 
 }

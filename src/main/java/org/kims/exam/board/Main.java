dd06602d1daed4dc4271e94c78f751b38a01fab8
neogoolboss <krossjsnet88@gmail.com>
2024-03-03 20:37:39 +0900

    C:/Program Files/Git/usr/article/write에 대한 명령어 처리

--- src/main/java/org/kims/exam/board/Main.java
+++ src/main/java/org/kims/exam/board/Main.java
@@ -14,7 +14,18 @@ public class Main {
         while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
-            System.out.printf("입력된 명령어 : %s\n", cmd);
+
+            if (cmd.equals("/usr/article/write")) {
+                System.out.println("== 게시물 등록 ==");
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
+
+                int id =1;
+
+                System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+            }
 
             if (cmd.equals("exit")){
                 System.out.println("프로그램을 종료합니다.");

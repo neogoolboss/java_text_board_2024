1573279231a5969251f10da40947d8a261cb77c4
neogoolboss <krossjsnet88@gmail.com>
2024-03-03 20:20:58 +0900

    시작 알림 메시지 출력

--- src/main/java/org/kims/exam/board/Main.java
+++ src/main/java/org/kims/exam/board/Main.java
@@ -2,10 +2,16 @@ package org.kims.exam.board;
 
 public class Main {
 
-
     public static void main(String[] args) {
+
+        System.out.println("== 게시판 v 0.1 ==");
+        System.out.println("== 게시판 시작 ==");
+        System.out.println("명령)");
+        System.out.println("== 게시판 실행 끝 =="
+        );
 
 
 
     }
+
 }
